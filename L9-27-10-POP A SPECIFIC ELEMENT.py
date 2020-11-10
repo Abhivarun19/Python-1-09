d150a4154096eac9cf240027b168b8f9d07c91ab
Abhivarun19 <70189892+Abhivarun19@users.noreply.github.com>
2020-11-10 10:32:38 +0530

    Add files via upload

---
+++ L9-27-10-POP A SPECIFIC ELEMENT.py
@@ -0,0 +1,9 @@
+#POP A SPECIFIC ELEMENT FROM STACK
+stack = ["NTR", "RAM", "RRR"] 
+print(stack) 
+# Remove 
+print(stack.pop(0)) 
+  
+print(stack) 
+  
+
---
+++ L9-27-10-PUSH A SPECIFIC ELEMENT.py
@@ -0,0 +1,9 @@
+#Push A ELEMENT IN STACK
+stack = ["NTR", "RAM", "RRR"]
+print (stack)
+# PUSH ELEMENT
+stack.append("MS") 
+stack.append("SAM") 
+print(stack) 
+
+  
